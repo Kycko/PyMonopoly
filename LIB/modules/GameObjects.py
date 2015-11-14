bca125c6a0df323fb4913c5624b805d9389eb2bb
Anthony Samartsev <kycok@archlinux.info>
2015-11-14 04:18:39 +0300

    sync GameField movement with background

--- LIB/modules/GameObjects.py
+++ LIB/modules/GameObjects.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 import Globals, pygame
-from GlobalFuncs import change_color_alpha
+from GlobalFuncs import change_color_alpha, slight_animation_count_pos
 
 class GameField():
     def __init__(self):
         self.cells = tuple([FieldCell(i) for i in range(40)])
+        self.move((-1820, 0))
+    def move(self, offset):
+        for cell in self.cells:
+            cell.change_new_pos(offset)
     def render(self):
         for cell in self.cells:
             cell.render()
@@ -12,29 +16,32 @@ class FieldCell():
     def __init__(self, number):
         if not number % 10:
             size = (80, 80)
-            x = (Globals.RESOLUTION[0]-600)/2+int(number in (0, 30))*521
+            x = 2120+int(number in (0, 30))*521
             y = 70+int(number in (0, 10))*521
         elif (number // 10) % 2:
             size = (80, 49)
             if number // 30:
-                x = (Globals.RESOLUTION[0]-600)/2+521
+                x = 2641
                 y = 101+(number % 10)*49
             else:
-                x = (Globals.RESOLUTION[0]-600)/2
+                x = 2120
                 y = 101+(10-number % 10)*49
         else:
             size = (49, 80)
             if number // 20:
-                x = (Globals.RESOLUTION[0]-600)/2+31+(number % 10)*49
+                x = 2151+(number % 10)*49
                 y = 70
             else:
-                x = (Globals.RESOLUTION[0]-600)/2+31+(10-number % 10)*49
+                x = 2151+(10-number % 10)*49
                 y = 591
         self.pos = (x, y)
         self.rect = pygame.Rect((0, 0), size)
         self.surf = pygame.Surface(size, pygame.SRCALPHA)
         self.change_color(Globals.COLORS['black'])
+    def change_new_pos(self, offset):
+        self.new_pos = (self.pos[0]+offset[0], self.pos[1]+offset[1])
     def change_color(self, color):
         pygame.draw.rect(self.surf, change_color_alpha(color, 104), self.rect, 0)
     def render(self):
+        self.pos = slight_animation_count_pos(self.new_pos, self.pos, 10, 15)
         Globals.screen.blit(self.surf, self.pos)
