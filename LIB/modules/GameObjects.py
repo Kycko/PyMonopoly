e4e8fac5258426a03fe8c44c3afef6791a143af4
Anthony Samartsev <kycok@archlinux.info>
2015-11-17 03:15:17 +0300

    rewrote GameObjects module: a separate surface for the whole GameField

--- LIB/modules/GameObjects.py
+++ LIB/modules/GameObjects.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-import Globals, FieldCellsData
+import Globals, FieldCellsData, pygame
 from GlobalFuncs import read_onboard_text, slight_animation_count_pos
-from pygame import draw, Rect, Surface
 
 class GameField():
     def __init__(self):
@@ -9,60 +8,66 @@ class GameField():
         groups = FieldCellsData.make_groups()
         group_symbols = FieldCellsData.make_group_symbols()
         group_colors = FieldCellsData.make_group_colors()
-        self.cells = tuple([FieldCell(onboard_text, groups[i], group_symbols[i], group_colors, i) for i in range(40)])
-        self.move((-1820, 0))
-    def move(self, offset):
-        for cell in self.cells:
-            cell.change_new_pos(offset)
+        self.cells = []
+        self.surf = pygame.Surface((601, 601), pygame.SRCALPHA)
+        for i in range(40):
+            size, pos = self.count_size_and_pos(i)
+            self.cells.append(FieldCell(onboard_text, groups[i], group_symbols[i], group_colors, i, size, pos))
+            self.change_color_for_a_cell(i, 'grey22')
+        self.pos = (2120, 70)
+        self.change_new_pos((-1820, 0))
+    def count_size_and_pos(self, num):
+        if not num % 10:
+            size = (80, 80)
+            x = int(num in (0, 30))*521
+            y = int(num in (0, 10))*521
+        elif (num // 10) % 2:
+            size = (80, 49)
+            if num // 30:
+                x = 521
+                y = 31+(num % 10)*49
+            else:
+                x = 0
+                y = 31+(10-num % 10)*49
+        else:
+            size = (49, 80)
+            if num // 20:
+                x = 31+(num % 10)*49
+                y = 0
+            else:
+                x = 31+(10-num % 10)*49
+                y = 521
+        return size, (x, y)
+    def change_new_pos(self, offset):
+        self.new_pos = (self.pos[0]+offset[0], self.pos[1]+offset[1])
     def render(self):
-        for cell in self.cells:
-            cell.render()
+        self.pos = slight_animation_count_pos(self.new_pos, self.pos, 10, 50)
+        Globals.screen.blit(self.surf, self.pos)
+    def change_color_for_a_cell(self, num, color):
+        self.cells[num].change_color(color)
+        self.surf.blit(self.cells[num].surf, self.cells[num].pos)
 class FieldCell():
-    def __init__(self, onboard_text, group, group_symbol, group_colors, number):
+    def __init__(self, onboard_text, group, group_symbol, group_colors, number, size, pos):
         #--- Onboard text
         self.group = group
         self.group_symbol = group_symbol
         if group in range(1, 9):
-            self.group_color = Globals.COLORS[group_colors[group-1]]
+            self.group_color = group_colors[group-1]
         else:
             self.group_color = None
         if number in onboard_text.keys():
             self.onboard_text = Globals.FONTS['ubuntu_16'].render(onboard_text[number], True, Globals.COLORS['black'])
         else:
             self.onboard_text = None
-        #--- Size and position
-        if not number % 10:
-            size = (80, 80)
-            x = 2120+int(number in (0, 30))*521
-            y = 70+int(number in (0, 10))*521
-        elif (number // 10) % 2:
-            size = (80, 49)
-            if number // 30:
-                x = 2641
-                y = 101+(number % 10)*49
-            else:
-                x = 2120
-                y = 101+(10-number % 10)*49
-        else:
-            size = (49, 80)
-            if number // 20:
-                x = 2151+(number % 10)*49
-                y = 70
-            else:
-                x = 2151+(10-number % 10)*49
-                y = 591
-        self.pos = (x, y)
-        #--- Rect and surface
-        self.rect = Rect((0, 0), size)
-        self.surf = Surface(size)
-        self.change_color(Globals.COLORS['grey22'])
-        self.RErender()
-    def change_new_pos(self, offset):
-        self.new_pos = (self.pos[0]+offset[0], self.pos[1]+offset[1])
+        #--- Position, rect and surface
+        self.pos = pos
+        self.rect = pygame.Rect((0, 0), size)
+        self.surf = pygame.Surface(size)
     def change_color(self, color):
         self.color = color
+        self.RErender()
     def RErender(self):
-        draw.rect(self.surf, self.color, self.rect, 0)
+        pygame.draw.rect(self.surf, Globals.COLORS[self.color], self.rect, 0)
         if self.group_color:
             rect = self.rect.copy()
             if self.group in (1, 2):
@@ -75,12 +80,9 @@ class FieldCell():
                 rect = rect.move(0, 60)
             elif self.group in (7, 8):
                 rect.w = 20
-            draw.rect(self.surf, self.group_color, rect, 0)
-            draw.rect(self.surf, Globals.COLORS['black'], rect, 1)
-        draw.rect(self.surf, Globals.COLORS['black'], self.rect, 1)
+            pygame.draw.rect(self.surf, Globals.COLORS[self.group_color], rect, 0)
+            pygame.draw.rect(self.surf, Globals.COLORS['black'], rect, 1)
+        pygame.draw.rect(self.surf, Globals.COLORS['black'], self.rect, 1)
         objects = [obj for obj in (self.onboard_text, self.group_symbol) if obj]
         for i in range(len(objects)):
             self.surf.blit(objects[i], ((self.rect.w-objects[i].get_width())/2, self.rect.h/4+16*i))
-    def render(self):
-        self.pos = slight_animation_count_pos(self.new_pos, self.pos, 10, 50)
-        Globals.screen.blit(self.surf, self.pos)
