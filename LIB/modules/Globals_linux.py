127eadc75b88105b306597a27562d74fb5119d51
Anthony Samartsev <kycok@archlinux.info>
2015-11-18 01:38:18 +0300

    Globals_linux.py update

--- LIB/modules/Globals_linux.py
+++ LIB/modules/Globals_linux.py
@@ -10,20 +10,33 @@ pygame.display.init()
 pygame.font.init()
 pygame.mixer.init()
 #--- Game version and resolution
-VERSION = '0.4-dev'
+VERSION = '0.4.1-dev'
 RESOLUTION = check_user_monitor(1200, 700)
 #--- Colors, directories, files, fonts, pictures and languages
 COLORS = {'black'               : pygame.Color('black'),
+          'brown45'             : pygame.Color(140, 77, 60),
           'light_brown'         : pygame.Color(160, 78, 61),
           'light_blue'          : pygame.Color(15, 124, 164),
+          'blue_magenta'        : pygame.Color(63, 16, 255),
+          'deep_sky_blue'       : pygame.Color(129, 187, 238),
+          'sky_blue'            : pygame.Color(186, 219, 238),
           'light_green'         : pygame.Color(126, 231, 85),
+          'deep_green'          : pygame.Color(95, 185, 93),
+          'dark_green'          : pygame.Color(47, 140, 46),
           'grey'                : pygame.Color('grey'),
+          'grey22'              : pygame.Color(200, 200, 200),
           'grey63'              : pygame.Color(95, 95, 95),
           'magenta'             : pygame.Color('magenta'),
+          'deep_magenta'        : pygame.Color(210, 59, 128),
+          'orange'              : pygame.Color(223, 138, 55),
+          'pink'                : pygame.Color(254, 126, 185),
+          'red27'               : pygame.Color(207, 41, 44),
           'light_red'           : pygame.Color(231, 85, 85),
           'red'                 : pygame.Color(255, 30, 30),
+          'deep_red'            : pygame.Color(200, 30, 30),
           'white'               : pygame.Color('white'),
-          'yellow'              : pygame.Color(228, 227, 17)}
+          'yellow'              : pygame.Color(228, 227, 17),
+          'deep_yellow'         : pygame.Color(235, 221, 57)}
 
 PLAYERS_COLORS = (COLORS['light_blue'],
                   COLORS['magenta'],
@@ -52,11 +65,14 @@ FONTS = {'ubuntu_11'    : pygame.font.Font(FILES['font_ubuntu'], 11),
          'ubuntu_24'    : pygame.font.Font(FILES['font_ubuntu'], 24),
          'ubuntu_32'    : pygame.font.Font(FILES['font_ubuntu'], 32),
          'ume_12'       : pygame.font.Font(FILES['font_ume'], 12),
-         'ume_16'       : pygame.font.Font(FILES['font_ume'], 16)}
+         'ume_16'       : pygame.font.Font(FILES['font_ume'], 16),
+         'ume_32'       : pygame.font.Font(FILES['font_ume'], 32),
+         'ume_40'       : pygame.font.Font(FILES['font_ume'], 40),
+         'ume_48'       : pygame.font.Font(FILES['font_ume'], 48)}
 
 PICS = {'appicon'               : DIRS['images'] + 'appicon.png',
-        'background'            : Sprite(((RESOLUTION[0]-1820)/2, -130), DIRS['images'] + 'background.jpg'),
-        'logo'                  : Sprite((RESOLUTION[0]/3, 120), DIRS['images_etc'] + 'logo.png')}
+        'background'            : DIRS['images'] + 'background.jpg',
+        'logo'                  : Sprite((RESOLUTION[0]/3, 120), DIRS['images_etc'] + 'logo.png', 50)}
 
 SOUNDS = {'music'               : pygame.mixer.music.load(DIRS['sounds'] + 'music.ogg'),
           'button-pressed'      : pygame.mixer.Sound(DIRS['sounds'] + 'button-pressed.wav')}
