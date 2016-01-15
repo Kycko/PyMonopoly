a394d6a89bc77c26b717747326775aa0b0521f71
Anthony Samartsev <kycok@archlinux.info>
2016-01-15 23:40:25 +0300

    bad deletion :/

--- LIB/modules/Players.py
+++ LIB/modules/Players.py
@@ -7,6 +7,7 @@ class Player():
         self.name = name
         self.color = color
         self.human = human
+        self.cur_field = 0
         self.game_piece = Surface((16, 16))
         draw.rect(self.game_piece, self.color, Rect((0, 0), (16, 16)))
         draw.rect(self.game_piece, Globals.COLORS['black'], Rect((0, 0), (16, 16)), 1)
