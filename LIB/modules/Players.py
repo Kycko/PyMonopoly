703e1bb2589ca308e9376948a0834aa421894015
Anthony Samartsev <kycok@archlinux.info>
2016-01-15 23:39:12 +0300

    A letters ahead player's pieces

--- LIB/modules/Players.py
+++ LIB/modules/Players.py
@@ -7,7 +7,7 @@ class Player():
         self.name = name
         self.color = color
         self.human = human
-        self.cur_field = 0
         self.game_piece = Surface((16, 16))
         draw.rect(self.game_piece, self.color, Rect((0, 0), (16, 16)))
         draw.rect(self.game_piece, Globals.COLORS['black'], Rect((0, 0), (16, 16)), 1)
+        self.game_piece.blit(Globals.FONTS['ubuntu_11'].render(self.name[0], True, Globals.COLORS['black']), (4, 1))
